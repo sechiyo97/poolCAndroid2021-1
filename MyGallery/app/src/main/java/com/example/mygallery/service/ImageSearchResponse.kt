a4a6dcc8c376e54ccbb53d3f0de986facdaccf8f
sechiyo97 <sechiyo97@yonsei.ac.kr>
2021-05-04 22:07:05 +0900

    life 키워드로 검색해서 받아오도록 설정

---
+++ MyGallery/app/src/main/java/com/example/mygallery/service/ImageSearchResponse.kt
@@ -0,0 +1,11 @@
+package com.example.mygallery.service
+
+import com.example.mygallery.ImageItem
+
+/**
+ * Created by seheelee on 2021-05-04.
+ */
+
+data class ImageSearchResponse(
+    val items : List<ImageItem>
+)
